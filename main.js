0516b97f242f803b590643982c46224eba814d90
favourA205 <favouragbejule@gmail.com>
2021-03-17 12:13:17 +0100

    More frontend functionality added

--- main.js
+++ main.js
@@ -89,10 +89,12 @@ sr.reveal('.portfolio__img',{interval:200})
 // SCROLL CONTACT
 
 sr.reveal('.contact__subtitle',{})
-sr.reveal('.contact__text',{interval:200})
-sr.reveal('.contact__input',{delay:400})
-sr.reveal('.contact__button',{delay:600})
-
+sr.reveal('.contact__text',{interval:150})
+// sr.reveal('.contact__input',{delay:400})
+sr.reveal('.contact__first-batch',{delay:400})
+sr.reveal('.contact__second-batch',{delay:500})
+sr.reveal('.contact__third-batch',{delay:600})
+// sr.reveal('.contact__button',{delay:600})
 
 
 
