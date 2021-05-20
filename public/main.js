99fb8d856ca0fca88b1e5603e31d8d98e8683e2d
favourA205 <favouragbejule@gmail.com>
2021-05-20 18:55:50 +0100

    More Frontend functionality added

--- public/main.js
+++ public/main.js
@@ -90,11 +90,9 @@ sr.reveal('.portfolio__img',{interval:200})
 
 sr.reveal('.contact__subtitle',{})
 sr.reveal('.contact__text',{interval:150})
-// sr.reveal('.contact__input',{delay:400})
 sr.reveal('.contact__first-batch',{delay:400})
 sr.reveal('.contact__second-batch',{delay:500})
 sr.reveal('.contact__third-batch',{delay:600})
-// sr.reveal('.contact__button',{delay:600})
 
 
 
